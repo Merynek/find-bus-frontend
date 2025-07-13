e4817e170c5d025ab18897d1dcc4aa5e05022659
Merynek <merynek@worldee.com>
2025-07-13 14:38:35 +0200

    clean

---
+++ src/app/(client)/create-trip/page.tsx
@@ -0,0 +1,7 @@
+import CreateTripPage from "@/src/components/pages/create-trip/create-trip-page";
+
+function PageWrapper()  {
+    return <CreateTripPage />;
+}
+
+export default PageWrapper;
--- src/components/pages/create-trip/create-trip-page.tsx
+++ src/components/pages/create-trip/create-trip-page.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import {useTranslate} from "@/src/hooks/translateHook";
-import React, {useRef} from "react";
+import React from "react";
 import {observer} from "mobx-react";
 import {CreateTripPageStore} from "./create-trip.page.store";
 import styles from "./create-trip-page.module.scss";
@@ -23,10 +25,11 @@ import {ROUTES} from "@/src/enums/router.enum";
 import {AppManager} from "@/src/singletons/app-manager";
 import {LayoutFlexColumn} from "@/src/components/components/layout/layout-flex-column/layout-flex-column";
 import {FlexGap} from "@/src/enums/layout.enum";
+import {useInit} from "@/src/hooks/lifecycleHooks";
 
 const CreateTripPage = observer(() => {
     const router = useRouter();
-    const _storeRef = useRef<CreateTripPageStore>(new CreateTripPageStore());
+    const _store = useInit(() => new CreateTripPageStore());
     const _configuration = useBean(AppConfiguration);
     const _appManager = useBean(AppManager);
     const {t} = useTranslate();
@@ -36,10 +39,10 @@ const CreateTripPage = observer(() => {
         <div className={styles.section}>
             <span>Kolik handikepovaných: </span>
             <NumberBox
-                value={_storeRef.current.trip.handicappedUserCount}
+                value={_store.trip.handicappedUserCount}
                 onChange={(val) => {
                     if (val) {
-                        _storeRef.current.trip.handicappedUserCount = val;
+                        _store.trip.handicappedUserCount = val;
                     }
                 }}
                 minValue={0}
@@ -48,10 +51,10 @@ const CreateTripPage = observer(() => {
         <div className={styles.section}>
             <span>Kdy nabídka končí: </span>
             <DatePicker
-                selected={_storeRef.current.trip.endOrder}
+                selected={_store.trip.endOrder}
                 onChange={(val) => {
                     if (val) {
-                        _storeRef.current.trip.endOrder = val;
+                        _store.trip.endOrder = val;
                     }
                 }}
                 placeholderText={"End"}
@@ -59,16 +62,16 @@ const CreateTripPage = observer(() => {
             />
         </div>
         <div>
-            {!_storeRef.current.endOrderIsValid && <p style={{color: "red"}}>Min EndOrder From Now is {cfg.minEndOrderFromNowInHours} in Hours</p>}
-            {!_storeRef.current.endOrderWithStartTripIsValid && <p style={{color: "red"}}>Min diff between EndOrder and StartTrip is {cfg.minDiffBetweenStartTripAndEndOrderInHours} in Hours</p>}
+            {!_store.endOrderIsValid && <p style={{color: "red"}}>Min EndOrder From Now is {cfg.minEndOrderFromNowInHours} in Hours</p>}
+            {!_store.endOrderWithStartTripIsValid && <p style={{color: "red"}}>Min diff between EndOrder and StartTrip is {cfg.minDiffBetweenStartTripAndEndOrderInHours} in Hours</p>}
         </div>
         <div className={styles.section}>
             <span>Pocet osob: </span>
             <NumberBox
-                value={_storeRef.current.trip.numberOfPersons}
+                value={_store.trip.numberOfPersons}
                 onChange={(val) => {
                     if (val) {
-                        _storeRef.current.trip.numberOfPersons = val;
+                        _store.trip.numberOfPersons = val;
                     }
                 }}
                 minValue={0}
@@ -77,8 +80,8 @@ const CreateTripPage = observer(() => {
         <div className={styles.section}>
             <span>Diety pro ridice: </span>
             <CheckBox
-                value={_storeRef.current.trip.dietForTransporter}
-                onChange={(val) => _storeRef.current.trip.dietForTransporter = val}
+                value={_store.trip.dietForTransporter}
+                onChange={(val) => _store.trip.dietForTransporter = val}
                 size={CheckBoxSize.MEDIUM}
             />
         </div>
@@ -87,13 +90,13 @@ const CreateTripPage = observer(() => {
                 return <CheckBox
                     key={index}
                     label={amenity}
-                    value={_storeRef.current.trip.amenities.indexOf(amenity) > -1}
+                    value={_store.trip.amenities.indexOf(amenity) > -1}
                     onChange={(val) => {
                         if (val) {
-                            _storeRef.current.trip.amenities.push(amenity);
+                            _store.trip.amenities.push(amenity);
                         } else {
-                            const index = _storeRef.current.trip.amenities.indexOf(amenity);
-                            removeOnIndex(_storeRef.current.trip.amenities, index);
+                            const index = _store.trip.amenities.indexOf(amenity);
+                            removeOnIndex(_store.trip.amenities, index);
                         }
                     }}
                     size={CheckBoxSize.MEDIUM}
@@ -102,30 +105,30 @@ const CreateTripPage = observer(() => {
         </div>
         <div className={styles.trips}>
             <div className={styles.stopsSection}>
-                {_storeRef.current.trip.routes.map((route, index) => {
+                {_store.trip.routes.map((route, index) => {
                     return <div key={index}>
                         <TripRouteCreate
                             route={route}
-                            trip={_storeRef.current.trip}
+                            trip={_store.trip}
                         />
                         <ButtonClick
                             size={ButtonSize.BY_CONTENT}
                             label={"Smazat"}
                             type={ButtonType.YELLOW}
                             onClick={() => {
-                                removeOnIndex(_storeRef.current.trip.routes, index);
+                                removeOnIndex(_store.trip.routes, index);
                             }}
                         />
                     </div>
                 })}
             </div>
-            {_storeRef.current.displayRecommendation && <div className={cn(styles.stopsSection, styles.recommendation)}>
+            {_store.displayRecommendation && <div className={cn(styles.stopsSection, styles.recommendation)}>
                 <p><b>ECONOMY</b> version for one driver</p>
-                {_storeRef.current.tripRecommendation === TripRecommendationType.ONE_DRIVER && <>{_storeRef.current.trip.routes.map((route, index) => {
+                {_store.tripRecommendation === TripRecommendationType.ONE_DRIVER && <>{_store.trip.routes.map((route, index) => {
                     return <div key={index}>
                         <TripRouteRecommendation
                             route={route}
-                            trip={_storeRef.current.trip}
+                            trip={_store.trip}
                         />
                         <div>
                             <p>DJ: {formatTimeForTrip(hoursToSeconds(route.currentDJ), t("component.trip.directionHours"), t("component.trip.directionMinutes"))}</p>
@@ -134,9 +137,9 @@ const CreateTripPage = observer(() => {
                         </div>
                     </div>
                 })}</>}
-                {_storeRef.current.tripRecommendation === TripRecommendationType.TWO_DRIVERS && <div>
-                    <p>REDUCE ROUTE: {_storeRef.current.reduceRoutesHours} Hours</p>
-                    <p>REDUCE TIME: {_storeRef.current.reduceTimeHours} Hours</p>
+                {_store.tripRecommendation === TripRecommendationType.TWO_DRIVERS && <div>
+                    <p>REDUCE ROUTE: {_store.reduceRoutesHours} Hours</p>
+                    <p>REDUCE TIME: {_store.reduceTimeHours} Hours</p>
                 </div>}
             </div>}
         </div>
@@ -146,7 +149,7 @@ const CreateTripPage = observer(() => {
                 label={"Přidat zastavku"}
                 type={ButtonType.YELLOW}
                 onClick={() => {
-                    _storeRef.current.trip.addRoute();
+                    _store.trip.addRoute();
                 }}
             />
             <ButtonClick
@@ -155,25 +158,25 @@ const CreateTripPage = observer(() => {
                 type={ButtonType.BLACK}
                 onClick={async () => {
                     let errors = "";
-                    _storeRef.current.validate();
-                    if (!_storeRef.current.isValid) {
-                        if (!_storeRef.current.placesAreSet) {
+                    _store.validate();
+                    if (!_store.isValid) {
+                        if (!_store.placesAreSet) {
                             errors += " @ All places are required, "
                         }
-                        if (!_storeRef.current.peopleCountIsValid) {
+                        if (!_store.peopleCountIsValid) {
                             errors += " @ Minimum count of people is 1, "
                         }
-                        if (!_storeRef.current.routesCountIsValid) {
+                        if (!_store.routesCountIsValid) {
                             errors += " @ Minimum count of routes is 1"
                         }
-                        if (!_storeRef.current.userSettings?.isValidForCreateInvoice) {
+                        if (!_store.userSettings?.isValidForCreateInvoice) {
                             errors += " @ settings are not valid"
                         }
                         alert(errors);
                     } else {
                         try {
                             _appManager.loading = true;
-                            await _storeRef.current.createTrip();
+                            await _store.createTrip();
                             _appManager.loading = false;
                             router.push(ROUTES.TRIP_LIST);
                         }
@@ -186,9 +189,9 @@ const CreateTripPage = observer(() => {
         </LayoutFlexColumn>
         <div style={{position: "relative", width: "100%", height: "300px"}}>
             <DirectionsMap
-                directions={_storeRef.current.trip.directions}
-                markers={_storeRef.current.trip.markers}
-                center={_storeRef.current.trip.stops.map(s => s.place.point).filter<GeoPoint>((p) : p is GeoPoint => p !== undefined)}
+                directions={_store.trip.directions}
+                markers={_store.trip.markers}
+                center={_store.trip.stops.map(s => s.place.point).filter<GeoPoint>((p) : p is GeoPoint => p !== undefined)}
                 initialView={{
                     zoom: 11
                 }}
