16ce20fc3aa15e430a130250ccf0cf6db6eb6d1b
Merynek <merynek@worldee.com>
2025-07-13 14:58:58 +0200

    clean

---
+++ src/app/(client)/(home-page)/page.tsx
@@ -0,0 +1,7 @@
+import HomePage from "@/src/components/pages/homepage/home-page";
+
+function PageWrapper()  {
+    return <HomePage />;
+}
+
+export default PageWrapper;
--- src/components/pages/homepage/home-page.tsx
+++ src/components/pages/homepage/home-page.tsx
@@ -2,11 +2,8 @@ import React from "react"
 import "./home.page.scss";
 import {observer} from "mobx-react";
 
-export interface IHomePageProps {
 
-}
-
-const HomePage = observer((props: IHomePageProps) => {
+const HomePage = observer(() => {
 
     return <div className={"layout"}>HOME</div>
 });
