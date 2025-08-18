d85d59f55690e631ea7817367d17c2e65d69e971
Merynek <merynek@worldee.com>
2025-08-18 15:33:51 +0200

    clean

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,20 @@
 import type { Preview } from '@storybook/nextjs'
 import {AppProvider} from "@/src/app/contexts/AppContext";
 import '@/src/styles/global.scss';
+import {NextIntlClientProvider} from "next-intl";
+import {LOCALES} from "@/src/utils/locale";
+import csMessages from "../messages/cs-cz.json";
+import enMessages from "../messages/en-us.json";
+
+type Messages = {
+  [LOCALES.cs_CZ]: typeof csMessages;
+  [LOCALES.en_US]: typeof enMessages;
+};
+
+const messages: Messages = {
+  [LOCALES.cs_CZ]: csMessages,
+  [LOCALES.en_US]: enMessages,
+};
 
 const preview: Preview = {
   parameters: {
@@ -14,11 +28,31 @@ const preview: Preview = {
       },
     },
   },
+  globalTypes: {
+    locale: {
+      description: 'Global locale for components',
+      defaultValue: LOCALES.cs_CZ,
+      toolbar: {
+        icon: 'globe',
+        items: [
+          { value: LOCALES.cs_CZ, title: 'Czech' },
+          { value: LOCALES.en_US, title: 'English' },
+        ],
+      },
+    },
+  },
   decorators: [
       (Story, context) => {
-        return <AppProvider>
-          <Story key={context.id} />
-        </AppProvider>
+        const { locale } = context.globals as { locale: keyof Messages };
+
+        return <NextIntlClientProvider
+            locale={locale}
+            messages={messages[locale]}
+        >
+          <AppProvider>
+            <Story key={context.id} />
+          </AppProvider>
+        </NextIntlClientProvider>
       }
   ]
 };
