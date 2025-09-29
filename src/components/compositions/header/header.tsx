32774a57665a4c1fcdadf688c0e04fa53c2bd86f
Merynek <merynek@worldee.com>
2025-09-29 10:59:11 +0200

    clean

--- src/components/compositions/header/header.tsx
+++ src/components/compositions/header/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import {ButtonLink, ButtonSize, ButtonType} from "../../components/button/button";
+import {ButtonClick, ButtonLink, ButtonSize, ButtonType} from "../../components/button/button";
 import {ROUTES} from "@/src/enums/router.enum";
 import {PageTabs} from "@/src/components/compositions/page-tabs/page-tabs";
 import {useLoggedUser} from "@/src/hooks/authenticationHook";
@@ -10,10 +10,13 @@ import {HeaderUserSection} from "@/src/components/compositions/header/header-use
 import {LayoutFlexRow} from "@/src/components/components/layout/layout-flex-row/layout-flex-row";
 import {FlexGap} from "@/src/enums/layout.enum";
 import {Logo} from "@/src/components/components/logo/logo";
+import {useGa} from "@/src/hooks/gaEventHook";
+import {GA_STORAGE} from "@/src/enums/ga.enums";
 
 export const Header = () => {
     const {user} = useLoggedUser();
     const {t} = useTranslate("component.header");
+    const {toggleStorageConsent} = useGa();
 
     const _signButtons = () => {
         return <LayoutFlexRow gap={FlexGap.SMALL_16}>
@@ -32,6 +35,19 @@ export const Header = () => {
         </LayoutFlexRow>
     }
 
+    const _gtmPermissions = () => {
+        return <ButtonClick
+            controlled={true}
+            size={ButtonSize.BY_CONTENT}
+            label={"GTM permissions"}
+            onClick={async () => {
+                toggleStorageConsent(GA_STORAGE.ANALYTICS_STORAGE, true);
+                toggleStorageConsent(GA_STORAGE.AD_STORAGE, true);
+            }}
+            type={ButtonType.BASE}
+        />
+    }
+
     return <header>
         <LayoutFlexRow>
             <Logo />
@@ -39,6 +55,7 @@ export const Header = () => {
                 <PageTabs user={user}/>
                 <div>|</div>
                 {user === null && _signButtons()}
+                {_gtmPermissions()}
                 {user !== null && <HeaderUserSection/>}
             </LayoutFlexRow>
         </LayoutFlexRow>
