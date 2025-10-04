82d8cc1750e29243018a41c48834e276f8c67bcd
Merynek <merynek@worldee.com>
2025-10-04 18:56:30 +0200

    vehicle rework

--- src/app/[locale]/create-trip/page.tsx
+++ src/app/[locale]/create-trip/page.tsx
@@ -1,10 +1,11 @@
 import CreateTripPage from "@/src/components/pages/create-trip/create-trip-page";
 import {AdminService} from "@/src/services/AdminService";
+import {AppBusinessConfigConverter} from "@/src/converters/admin/app-business-config-converter";
 
 async function PageWrapper()  {
     const config = await AdminService.getAppBusinessConfig();
     return <CreateTripPage
-        cfg={config}
+        cfg={AppBusinessConfigConverter.toJson(config)}
     />;
 }
 
