5307751b61f927afdbd8238d7e082ff744468353
Merynek <merynek@worldee.com>
2025-10-08 16:09:25 +0200

    vehicle rework

--- src/components/pages/vehicles/vehicles.page.tsx
+++ src/components/pages/vehicles/vehicles.page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import {VehicleDetail} from "../../compositions/vehicle/detail-list/vehicle-detail-list";
 import {ButtonLink, ButtonSize, ButtonType} from "../../components/button/button";
 import {ROUTES, URL_PARAMS} from "@/src/enums/router.enum";
 import {LayoutFlexColumn} from "@/src/components/components/layout/layout-flex-column/layout-flex-column";
 import AddVehicleButton from "@/src/components/pages/vehicles/add-vehicle-button";
 import {Vehicle} from "@/src/data/vehicle/vehicle";
+import {LayoutFlexRow} from "@/src/components/components/layout/layout-flex-row/layout-flex-row";
+import {Text} from "@/src/components/components/texts/text";
+import {FontSize, FontWeight} from "@/src/components/components/texts/textStyles";
+import {FlexGap} from "@/src/enums/layout.enum";
 
 export interface IVehiclesPageProps {
     vehicles: Vehicle[];
@@ -14,12 +17,19 @@ const VehiclesPage = (props: IVehiclesPageProps) => {
     const {vehicles} = props;
 
     const _renderList = () => {
-        return <div>
-            <AddVehicleButton />
+        return <LayoutFlexColumn gap={FlexGap.LARGE_32}>
             {vehicles.map((vehicle => {
-                return <div key={vehicle.id}>
-                    <VehicleDetail
-                        vehicle={vehicle}
+                return <LayoutFlexRow key={vehicle.id} gap={FlexGap.MEDIUM_24}>
+                    <Text text={vehicle.id.toString()} fontSize={FontSize.M_24} fontWeight={FontWeight.SEMIBOLD} />
+                    <Text text={vehicle.name} fontSize={FontSize.M_24} fontWeight={FontWeight.SEMIBOLD} />
+                    <ButtonLink
+                        route={{
+                            route: ROUTES.VEHICLE_DETAIL,
+                            params: { [URL_PARAMS.VEHICLE_ID]: vehicle.id.toString() }
+                        }}
+                        label={"Detail"}
+                        type={ButtonType.BLACK}
+                        size={ButtonSize.BUTTON_SIZE_M}
                     />
                     <ButtonLink
                         route={{
@@ -30,12 +40,13 @@ const VehiclesPage = (props: IVehiclesPageProps) => {
                         type={ButtonType.BLACK}
                         size={ButtonSize.BUTTON_SIZE_M}
                     />
-                </div>
+                </LayoutFlexRow>
             }))}
-        </div>
+        </LayoutFlexColumn>
     }
 
     return <LayoutFlexColumn>
+        <AddVehicleButton />
         {_renderList()}
     </LayoutFlexColumn>
 };
