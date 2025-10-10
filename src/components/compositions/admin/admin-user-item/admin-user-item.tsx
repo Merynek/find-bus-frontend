7075d8821b65888770a5b3ac661f12fd84f24ff9
Merynek <merynek@worldee.com>
2025-10-10 22:54:46 +0200

    vehicle rework

--- src/components/compositions/admin/admin-user-item/admin-user-item.tsx
+++ src/components/compositions/admin/admin-user-item/admin-user-item.tsx
@@ -100,7 +100,7 @@ export const AdminUserItem = (props: IAdminUserItemProps) => {
             <Group title={"Vehicles"}>
                 {user.vehicles.map(v => {
                     return <LayoutFlexRow gap={FlexGap.SMALL_16} key={v.id}>
-                        <Text text={`${v.name} - ${v.status}`} fontSize={FontSize.M_22} />
+                        <Text text={`ID: ${v.id}: ${v.name} - ${v.status}`} fontSize={FontSize.M_22} />
                         <ButtonLink
                             route={{
                                 route: ROUTES.ADMIN_VEHICLE,
