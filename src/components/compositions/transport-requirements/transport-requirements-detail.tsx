7329a84828e1d5b6031e01e0e16ca1c24efe41af
Merynek <merynek@worldee.com>
2025-11-25 12:36:50 +0100

    clean

--- src/components/compositions/transport-requirements/transport-requirements-detail.tsx
+++ src/components/compositions/transport-requirements/transport-requirements-detail.tsx
@@ -3,6 +3,9 @@ import {LayoutFlexColumn} from "@/src/components/components/layout/layout-flex-c
 import {LayoutFlexRow} from "@/src/components/components/layout/layout-flex-row/layout-flex-row";
 import {FlexGap} from "@/src/enums/layout.enum";
 import {TransportRequirements} from "@/src/data/transportRequirements";
+import {TransportRequirementsType} from "@/src/api/openapi";
+import {Text} from "@/src/components/components/texts/text";
+import {FontSize} from "@/src/components/components/texts/textStyles";
 
 export interface ITransportRequirementsDetailProps {
     requirements: TransportRequirements;
@@ -21,6 +24,21 @@ export const TransportRequirementsDetail = (props: ITransportRequirementsDetailP
         </div>
     }
 
+    const renderDocumentsByType = (type: TransportRequirementsType) => {
+        const documents = requirements.documents.filter(d => d.type === type);
+
+        return <LayoutFlexColumn>
+            <Text text={type} fontSize={FontSize.M_22} />
+            <LayoutFlexRow canWrap={true} gap={FlexGap.SMALL_16}>
+                {documents.map(document => {
+                    return <div key={document.id}>
+                        {document.image?.path && renderImage(document.image.path)}
+                    </div>
+                })}
+            </LayoutFlexRow>
+        </LayoutFlexColumn>
+    }
+
     return <LayoutFlexColumn>
         <LayoutFlexColumn>
             <div style={{backgroundColor: "aquamarine"}}>
@@ -31,13 +49,9 @@ export const TransportRequirementsDetail = (props: ITransportRequirementsDetailP
                 <span>{requirements.concessionNumber}</span>
             </div>
         </LayoutFlexColumn>
-        <LayoutFlexRow canWrap={true} gap={FlexGap.SMALL_16}>
-            {requirements.documents.map(document => {
-                return <div key={document.id}>
-                    <span>{document.type}:</span>
-                    {document.image?.path && renderImage(document.image.path)}
-                </div>
-            })}
-        </LayoutFlexRow>
+        <LayoutFlexColumn>
+            {renderDocumentsByType(TransportRequirementsType.CONCESSION)}
+            {renderDocumentsByType(TransportRequirementsType.BUSINESS_RISK)}
+        </LayoutFlexColumn>
     </LayoutFlexColumn>
 };
