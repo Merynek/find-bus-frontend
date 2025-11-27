f7ba07e026a0742ea821b0e1493e5962d4fdeae5
Merynek <merynek@worldee.com>
2025-11-27 18:10:48 +0100

    styles

--- src/components/compositions/sign/sign-modal/sign-modal.tsx
+++ src/components/compositions/sign/sign-modal/sign-modal.tsx
@@ -1,6 +1,7 @@
 import {ModalDialog} from "@/src/components/components/modal-dialog/modal-dialog";
 import React from "react";
 import {SignUpForm} from "@/src/components/compositions/sign/sign-up/sign-up-form";
+import {ModalSize} from "@/src/components/components/modal/modal";
 
 export interface ISignModalProps {
     afterRegistration?: (email: string) => Promise<void>;
@@ -16,7 +17,9 @@ export const SignModal = (props: ISignModalProps) => {
             headerText={"----"}
             onClose={onClose}
         >
-            <SignUpForm afterRegistration={afterRegistration} />
+            <div style={{padding: "50px"}}>
+                <SignUpForm afterRegistration={afterRegistration} />
+            </div>
         </ModalDialog>
     )
 }
